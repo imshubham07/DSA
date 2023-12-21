7e26deb02dc54f4a0b6f35399cdd1da3e45eb51a
imshubham07 <mrshubhamdubey20@gmail.com>
2023-12-21 22:10:13 +0530

    Binary Search & its Question

--- Searching & Shorting/Binary.cpp
+++ Searching & Shorting/Binary.cpp
@@ -5,22 +5,23 @@ int binarySearch(int arr[],int size,int target){
     int start = 0;
     int end = size-1;
 
-    int mid = (start+end)/2;
+    int mid = start + (end - start)/2;
 
     while (start<=end )
     {
         int element = arr[mid];
         if (element == target)
         {
+            //element Found on target
             return mid;
         }
         else if (target<element)
         {
-            //Searching in Left side 
+            //Search in Left side 
             end = mid - 1;
         }
         else{
-            //searching in Right side
+            //search in Right side
             start = mid +1;
         }
         mid = (start + end )/2;
@@ -32,19 +33,37 @@ int binarySearch(int arr[],int size,int target){
 
 
 int main(){
-     int arr[]= {1,2,3,4,5,6,7,8,9,10};
-     int target = 5;
-     int size = 10;
+    //  int arr[]= {1,2,3,4,5,6,7,8,9,10};
+    //  int target = 5;
+    //  int size = 10;
 
 
 
-     int indexOfTarget = binarySearch(arr,size,target);
+    //  int indexOfTarget = binarySearch(arr,size,target);
      
 
-     if (indexOfTarget == -1){
-         cout<<"Element not found";
-     }
-     else{
-         cout<<"Target found at "<<indexOfTarget<<"index"<<endl;
-     }
+    //  if (indexOfTarget == -1){
+    //      cout<<"Element not found";
+    //  }
+    //  else{
+    //      cout<<"Target found at "<<indexOfTarget<<"index"<<endl;
+    //  }
+
+    vector<int>v {1,2,3,4,5,6};
+    int arr[] = {1,2,3,4,5,6,7};
+    int size = 7;
+
+
+
+    //binary_search use in cpp than you must be include (<algorithm>) header file
+    if (binary_search(arr, arr + size ,7))
+    {
+        cout<<"found"<<endl;
+    }
+    else
+    {
+        cout<<"not Found"<<endl;
+    }
+    
+    
 }
---
+++ Searching & Shorting/Binary_Ques.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// //Q2. find Occurence the element through binary Search
+
+// int LastOccurence(vector<int>v , int targets){
+//     int s = 0;
+//     int e = v.size() - 1;
+//     int mid = s + (e - s)/2;
+//     int ans = -1;
+
+//     while (s<=e)
+//     {
+//         if ( v[mid] == targets  )
+//         {
+//            // ans store and than right
+//            ans = mid;
+//            //search right
+//            s = mid + 1;
+//         }
+//         else if (targets < v[mid])
+//         {
+//             //Left Side Searching
+//             e = mid - 1;
+//         }
+//         else if (targets > v[mid])
+//         {
+//             //Right side search
+//             s = mid + 1;
+//         }
+//         mid = s + (e - s)/2;  
+//     }
+//     return ans;
+    
+// }
+
+//Q1. find Occurence the element through binary Search
+// int FirstOccurence(vector<int>arr, int target){
+//     int s = 0;
+//     int e = arr.size() - 1;
+//     int mid = s + (e - s)/2;
+//     int ans = -1;
+
+//     while (s<=e)
+//     {
+//         if ( arr[mid] == target  )
+//         {
+//            // ans store o
+//            ans = mid;
+//             //search Left side
+//            e = mid - 1;
+//         }
+//         else if (target> arr[mid])
+//         {
+//             //right side search
+//             s = mid + 1;
+//         }
+//         else if (target < arr[mid])
+//         {
+//             //left side search
+//             e = mid - 1;
+//         }
+//         mid = s + (e - s)/2;  
+//     }
+//     return ans;  
+// }
+
+
+int main(){
+
+    
+    //Q1. find Occurence the element through binary Search
+
+    //  vector<int> r{1,2,3,4,4,4,4,8,8,8,8,9};
+    //  int target = 4;
+    //  int indexofFirstOcc = FirstOccurence(r,target);
+
+    //  cout<< "ans is "<<indexofFirstOcc<<endl;
+
+    // //finding firstOccurence than you can use inbuilt function 
+    // auto ans2 = lower_bound (r.begin(), r.end(), 8) ;
+    // cout<< "ans2 is  "<<ans2-r.begin()<<endl;
+
+
+
+
+
+
+
+    //Q2. find Occurence the element through binary Search
+    // vector<int>v{1,2,3,7,7,7,7,7,7,7,8,9};
+    // int targets = 7;
+    // // int indexOfLastOcc = LastOccurence(v,targets);
+
+    // //cout<< "ans is "<<indexOfLastOcc<<endl;
+    // auto ans2 = upper_bound (v.begin(), v.end(), 7) ;
+    // cout<< "ans2 is  "<<ans2-v.begin()<<endl;
+
+
+    //Q3. Find missing number of given element 
+
+    vector<int>arr{1,2,3,4,6,7,8,9};
+}

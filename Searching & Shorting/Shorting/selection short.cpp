872196ad46f240580993266b65b8c4b8766789b9
imshubham07 <imshubham07>
2024-01-09 16:45:33 +0530

    Selection Short

--- Searching & Shorting/Shorting/selection short.cpp
+++ Searching & Shorting/Shorting/selection short.cpp
@@ -7,21 +7,21 @@ int main(){
     vector<int>arr{1,5,4,78,6,47,3,5,7,9,2,7,62,4};
     int n = arr.size();
 
-// Outer loop will run only till second last element 
+// Outer loop -> (n-1) round
     for (int i = 0; i < n-1; i++)
     {
         int minIndex = i; 
-        // We will search in the next index
+        // Inner loop -> index of minimum element in range loop
         for (int  j = i+1; j < n; j++)
         {
             //find min element
             if (arr[j]<arr[minIndex])
             {
-                //Will store a new element when found
+                //new minimum, than store
                 minIndex = j;
             }
         }
-        //swap the number with minimum element
+        //swap 
         swap(arr[i],arr[minIndex]);
     }
 
